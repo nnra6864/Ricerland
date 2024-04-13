f392e01a4590e754b04030b7f35cb0c87369e036
nnra6864 <enenragd@gmail.com>
2024-04-13 21:20:10 +0200

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,7 @@ if status is-interactive
  	end
 end
 alias nv='~/.local/share/applications/neovide.AppImage'
+function 8k
+  ffmpeg -i $argv[1] -vf scale=7680:4320 -c:v libx265 -crf 23 -c:a copy $argv[2]
+end
 fish_vi_key_bindings
