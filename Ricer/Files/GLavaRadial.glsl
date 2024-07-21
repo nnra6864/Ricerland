96bd6c711b3073e8f49f315773c0cf167585c335
nnra6864 <enenragd@gmail.com>
2024-07-21 22:10:24 +0200

    Added gtk and glava to ricer configs

--- Ricer/Files/GLavaRadial.glsl
+++ Ricer/Files/GLavaRadial.glsl
@@ -3,7 +3,7 @@
 /* center line thickness (pixels) */
 #define C_LINE 5
 /* outline color */
-#define OUTLINE #81A1C1
+#define OUTLINE #ricer.col.outline.hex
 /* number of bars (use even values for best results) */
 #define NBARS 1024
 /* width (in pixels) of each bar*/
@@ -15,7 +15,7 @@
 /* Amplify magnitude of the results each bar displays */
 #define AMPLIFY 500
 /* Bar color */ 
-#define COLOR (#ECEFF4)
+#define COLOR (#ricer.col.text.hex)
 /* Angle (in radians) for how much to rotate the visualizer */
 #define ROTATE (PI / 2)
 /* Whether to switch left/right audio buffers */
