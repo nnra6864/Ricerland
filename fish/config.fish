6abce98d76b7e1a1793a64cec38cc5a89571d5c4
nnra6864 <enenragd@gmail.com>
2024-10-06 21:21:13 +0200

    Added a function to combine mics

--- fish/config.fish
+++ fish/config.fish
@@ -167,5 +167,31 @@ function mp3
     end
 end
 
+function combine_mics
+    # Create a new virtual source that other apps can use as input
+    set SOURCE_NAME "combined_mic"
+
+    # First create a null sink that will serve as our mixing point
+    pactl load-module module-null-sink sink_name="$SOURCE_NAME"_mix sink_properties=device.description="$SOURCE_NAME"_mix
+
+    # Create a virtual source that monitors our null sink
+    # This makes the mixed audio available as an input
+    pactl load-module module-virtual-source source_name=$SOURCE_NAME master="$SOURCE_NAME"_mix.monitor source_properties=device.description="Combined Microphones"
+
+    # Get a list of all audio input sources
+    set SOURCES (pactl list sources short | awk '/input/ {print $2}')
+
+    # Loop through each audio input source and create a loopback to our mixing sink
+    for SOURCE in $SOURCES
+        # Skip any virtual sources we just created to avoid feedback loops
+        if test "$SOURCE" != "*$SOURCE_NAME*" -a "$SOURCE" != "*monitor*"
+            pactl load-module module-loopback source=$SOURCE sink="$SOURCE_NAME"_mix latency_msec=1
+        end
+    end
+
+    echo "All microphone inputs have been combined into the virtual source '$SOURCE_NAME'"
+    echo "You can now select '$SOURCE_NAME' as an input device in your applications"
+end
+
 zoxide init fish | source
 fish_vi_key_bindings
