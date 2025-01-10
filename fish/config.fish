54b22aa5862e44b56587c016d674109c590e1f8d
nnra6864 <enenragd@gmail.com>
2025-01-10 15:09:32 +0100

    Added the hdr alias to fish cfg

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,7 @@ alias doas='sudo'
 alias nlear='clear; neofetch'
 alias py='python'
 alias timeshit='timeshift'
+alias hdr='ENABLE_HDR_WSI=1 mpv --vo=gpu-next --target-colorspace-hint --gpu-api=vulkan --gpu-context=waylandvk --fullscreen'
 
 # paths
 alias cfg='cd ~/.config/'
