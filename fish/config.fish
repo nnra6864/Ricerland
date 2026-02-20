eff8bd9e1a94363485fb3a95943afa85a74bf061
nnra6864 <nnra2210@gmail.com>
2026-02-20 18:24:52 +0100

    No longer updating nvidia drivers in update since I use cachyos ones

--- fish/config.fish
+++ fish/config.fish
@@ -56,11 +56,11 @@ function update
 	and hyprpm update
 
 	# Update nv drivers
-	cd ~/Packages/nvidia-all
-	and git pull
-    and notify-send "Update" "Input required for nvidia drivers"
-    and paplay /usr/share/sounds/freedesktop/stereo/message.oga
-	and makepkg -si
+	#cd ~/Packages/nvidia-all
+	#and git pull
+    #and notify-send "Update" "Input required for nvidia drivers"
+    #and paplay /usr/share/sounds/freedesktop/stereo/message.oga
+	#and makepkg -si
 
 	# Update flatpak
 	and flatpak update -y
