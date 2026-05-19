7ffc6ae2df27e490c61043b44836e468409c1d10
nnra6864 <nnra2210@gmail.com>
2026-05-19 17:25:48 +0200

    Deleted an obsolete command

--- hypr/hyprland/env/init.lua
+++ hypr/hyprland/env/init.lua
@@ -2,8 +2,3 @@ require("hyprland.env.hyprland")
 require("hyprland.env.applications")
 require("hyprland.env.appearance")
 require("hyprland.env.nvidia")
-
--- Export all env vars
--- hl.on("config.reloaded", function()
---     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
--- end)
