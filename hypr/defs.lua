212f2f330812719eddb17cb7fde3e8d042948fd0
nnra6864 <nnra2210@gmail.com>
2026-05-19 15:51:18 +0200

    Restructured defs

--- hypr/defs.lua
+++ hypr/defs.lua
@@ -1,61 +1,164 @@
 local M = {}
 
--- Apps
+-- Main mod
 M.main_mod = "SUPER"
-M.terminal = "ghostty"
-M.browser = "zen-browser"
-M.file_manager = "yazi"
 
--- Appearance
-M.active_opacity = 1.0
-M.inactive_opacity = 1.0
-M.inactive_dimming = 0.6
-M.gaps_in = 5
-M.gaps_out = 10
-M.blur = true
-M.blur_size = 20
-M.blur_passes = 4
-M.shadow_col = "#081D2680"
-M.shadow_inactiveCol = "#081D2680"
-M.rounding = 10
-M.rounding_power = 2
+-- Apps
+M.apps = {
+    terminal     = "ghostty",
+    browser      = "zen-browser",
+    file_manager = "yazi"
+}
+
+-- Opacity
+M.opacity = {
+    active   = 1.0,
+    inactive = 1.0,
+}
+
+-- Dimming
+M.dimming = {
+    modal    = true,
+    inactive = true,
+    strength = 0.6,
+    special  = 0.2,
+    around   = 0.4
+}
+
+-- Gaps
+M.gaps = {
+    inner = 5,
+    outer = 10
+}
+
+-- Shadow
+M.shadow = {
+    col         = "#081D2680",
+    inactiveCol = "#081D2680"
+}
 
 -- Border
-M.border_size = 2
-M.border_active_rotation = 90
-M.border_inactive_rotation = -90
-M.border_active_col = { colors = { "#266F7F", "#A3C5CC" }, M.borderActiveRotation }
-M.border_inactive_col = { colors = { "#1E444C", "#899699" }, M.borderInactiveRotation }
-M.border_fade_animation = 1
-M.border_fade_duration = 1
-M.border_fade_bezier = "quadOut"
-M.border_rotation_animation = 1
-M.border_rotation_duration = 30
-M.border_rotation_bezier = "linear"
-M.border_rotation_type = "loop"
+M.border = {
+    rounding       = 10,
+    rounding_power = 2,
+    size           = 2,
+
+    -- Color
+    active_col   = { colors = { "#266F7F", "#A3C5CC" }, M.borderActiveRotation },
+    inactive_col = { colors = { "#1E444C", "#899699" }, M.borderInactiveRotation },
+
+    -- Rotation
+    active_rotation    = 90,
+    inactive_rotation  = -90,
+    rotation_animation = 1,
+    rotation_duration  = 30,
+    rotation_bezier    = "linear",
+    rotation_type      = "loop",
+
+    -- Fade
+    fade_animation = 1,
+    fade_duration  = 1,
+    fade_bezier    = "quadOut"
+}
+
+
+-- Blur
+M.blur = {
+    enabled       = true,
+    popups        = true,
+    input_methods = true,
+    special       = false,
+
+    ignore_opacity             = true,
+    popups_ignore_alpha        = true,
+    input_methods_ignore_alpha = true,
+
+    new_optimizations = true,
+    xray              = true,
+
+    size              = 20,
+    passes            = 4,
+    noise             = 0.02,
+    contrast          = 1,
+    brightness        = 0.6,
+    vibrancy          = 1,
+    vibrancy_darkness = 0
+}
+
+-- Shadow
+M.shadow = {
+    enabled = true,
+    sharp   = false,
+
+    range        = 4,
+    render_power = 3,
+    scale        = 1,
+
+    color          = "#000000",
+    color_inactive = "#000000",
+
+    offset = { 0, 0 }
+}
+
+-- Glow
+M.shadow = {
+    enabled = false,
+
+    range        = 10,
+    render_power = 3,
+
+    color          = "#000000",
+    color_inactive = "#000000",
+}
 
 -- Font
-M.font = "Maple Mono NF CN"
-M.fontSize = 13
+M.font = {
+    family = "Maple Mono NF CN",
+    size   = 13
+}
 
 -- Cursor
-M.cursor = "Bibata-Modern-Ice"
-M.cursor_size = 24
-M.cursor_rot = 21
+M.cursor = {
+    name = "Bibata-Modern-Ice",
+    size = 24
+}
 
 -- Theme
-M.theme = "oomox-Ricer"
-M.background = os.getenv("HOME") .. "/.config/Backgrounds/Kyanos/Misty_Cloudy_Mountain.jpg"
-M.bg_col = "#081D26"
-M.fg_col = "#A3C5CC"
-M.bg_col_a = "#081D2680"
-M.termColors = "di=#A3C5CC:fi=#A3C5CC:ln=#267F6F:pi=#267F6F:so=#266F7F:bd=#266F7F:cd=#266F7F:or=#7F2626:mi=#7F2626:ex=#267F32"
+M.theme = {
+    background = os.getenv("HOME") .. "/.config/Backgrounds/Kyanos/Misty_Cloudy_Mountain.jpg",
+
+    qt = {
+        platform_theme            = "qt6ct",
+        quick_controls_style      = "org.kde.desktop",
+        disable_window_decoration = 1,
+        scaling                   = 1,
+        screen_scaling            = 0
+    },
+
+    gtk = {
+        name    = "oomox-Ricer",
+        scaling = 1
+    },
+
+    color = {
+        bg   = "#081D26",
+        fg   = "#A3C5CC",
+        bg_a = "#081D2680",
+        ls   = "di=#A3C5CC:fi=#A3C5CC:ln=#267F6F:pi=#267F6F:so=#266F7F:bd=#266F7F:cd=#266F7F:or=#7F2626:mi=#7F2626:ex=#267F32"
+    }
+}
 
 -- Animation
-M.animation_duration_multiplier = 1
+M.animation ={
+    enabled = true,
+    workspace_wraparound = false,
+    duration_multiplier = 1
+}
 
 -- Sound
-M.screenshot_sound = os.getenv("HOME") .. "/.config/SFX/Select.wav"
+M.sound = {
+    screenshot = os.getenv("HOME") .. "/.config/SFX/Select.wav"
+}
 
 -- Overrides
 local overrides_path = package.searchpath("overrides", package.path)
