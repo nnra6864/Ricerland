387d87d14c2ed9ccbfb2684aa578bdd4623074d8
nnra6864 <nnra2210@gmail.com>
2026-05-19 15:51:49 +0200

    blur.lua

---
+++ hypr/hyprland/decoration/blur.lua
@@ -0,0 +1,24 @@
+local defs = require("defs")
+
+hl.config({
+    decoration = {
+        blur ={
+            enabled = defs.blur.enabled,
+            size = defs.blur.size,
+            passes = defs.blur.passes,
+            ignore_opacity = defs.blur.ignore_opacity,
+            new_optimizations = defs.blur.new_optimizations,
+            xray = defs.blur.xray,
+            noise = defs.blur.noise,
+            contrast = defs.blur.contrast,
+            brightness = defs.blur.brightness,
+            vibrancy = defs.blur.vibrancy,
+            vibrancy_darkness = defs.blur.vibrancy_darkness,
+            special = defs.blur.special,
+            popups = defs.blur.popups,
+            popups_ignore_alpha = defs.blur.popups_ignore_alpha,
+            input_methods = defs.blur.input_methods,
+            input_methods_ignore_alpha = defs.blur.input_methods_ignore_alpha
+        }
+    }
+})
