030621b31d138e4697addf538eefc72a663f50c4
nnra6864 <nnra2210@gmail.com>
2026-05-19 15:51:59 +0200

    Updated env.lua

--- hypr/hyprland/env.lua
+++ hypr/hyprland/env.lua
@@ -1,52 +1,58 @@
+local defs = require("defs")
+
 -- Hyprland
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 
+-- Applications
+hl.env("TERMINAL", defs.apps.terminal)
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("CLUTTER_BACKEND", "wayland")
+
 -- QT
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("QT_QUICK_CONTROLS_STYLE", "org.kde.desktop")
-hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
-hl.env("QT_SCALE_FACTOR", "1")
-hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_QPA_PLATFORMTHEME", defs.theme.qt.platform_theme)
+hl.env("QT_QUICK_CONTROLS_STYLE", defs.theme.qt.quick_controls_style)
+hl.env("QT_SCALE_FACTOR", defs.theme.qt.scaling)
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", defs.theme.qt.screen_scaling)
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", defs.theme.qt.disable_window_decoration)
 
--- Theming
-hl.env("GTK_THEME", "$theme")
-hl.env("GDK_SCALE", "1")
-hl.env("CLUTTER_BACKEND", "wayland")
-hl.env("LS_COLORS", "di=#A3C5CC:fi=#A3C5CC:ln=#267F6F:pi=#267F6F:so=#266F7F:bd=#266F7F:cd=#266F7F:or=#7F2626:mi=#7F2626:ex=#267F32")
+-- GTK
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("GTK_THEME", defs.theme.gtk.name)
+hl.env("GDK_SCALE", defs.theme.gtk.scaling)
 
 -- Cursor
-hl.env("XCURSOR_THEME", "$cursor")
-hl.env("XCURSOR_SIZE", "$cursorSize")
-hl.env("HYPRCURSOR_THEME", "$cursor")
-hl.env("HYPRCURSOR_SIZE", "$cursorSize")
+hl.env("XCURSOR_THEME", defs.cursor.name)
+hl.env("XCURSOR_SIZE", defs.cursor.size)
+hl.env("HYPRCURSOR_THEME", defs.cursor.name)
+hl.env("HYPRCURSOR_SIZE", defs.cursor.size)
 
--- Applications
-hl.env("TERMINAL", "$terminal")
+-- Mozilla
+hl.env("MOZ_ENABLE_WAYLAND", "1")
 hl.env("MOZ_WAYLAND_USE_VAAPI", "1")
-hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
-hl.env("_JAVA_AWT_WM_NONREPARENTING", "1")
--- Required for gparted and some other x11 apps
-hl.env("XAUTHORITY", os.getenv("HOME") .. "/.Xauthority")
+hl.env("MOZ_DBUS_REMOTE", "1")
+hl.env("MOZ_ACCELERATED", "1")
+hl.env("MOZ_WEBRENDERER", "1")
+
 -- Makes WiVRn recentering work
 hl.env("OXR_RECENTER_STAGE", "1")
 
--- Nvidia
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("GBM_BACKEND", "nvidia-drm")
-hl.env("EGL_PLATFORM", "wayland")
-hl.env("WLR_USE_LIBINPUT", "1")
-hl.env("__GL_VRR_ALLOWED", "0")
-hl.env("__GL_GSYNC_ALLOWED", "1")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("NVD_BACKEND", "direct")
+-- Java
+hl.env("_JAVA_AWT_WM_NONREPARENTING", "1")
 
--- Accessibility, needed for hints(https://github.com/AlfredoSequeida/hints)
-hl.env("ACCESSIBILITY_ENABLED", "1")
-hl.env("GTK_MODULES", "gail:atk-bridge")
-hl.env("OOO_FORCE_DESKTOP", "gnome")
-hl.env("GNOME_ACCESSIBILITY", "1")
-hl.env("QT_ACCESSIBILITY", "1")
-hl.env("QT_LINUX_ACCESSIBILITY_ALWAYS_ON", "1")
+-- Required for gparted and some other x11 apps
+hl.env("XAUTHORITY", os.getenv("HOME") .. "/.Xauthority")
+
+-- Term colors
+hl.env("LS_COLORS", defs.theme.color.ls)
+
+-- Nvidia
+hl.env("NVD_BACKEND", "direct")
+hl.env("GBM_BACKEND", "nvidia-drm")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("__GL_GSYNC_ALLOWED", "1")
+hl.env("__GL_VRR_ALLOWED", "0")
