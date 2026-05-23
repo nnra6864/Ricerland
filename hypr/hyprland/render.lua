4b774f2e10b78272bf5dd0851a0bdcd9fed98748
nnra6864 <nnra2210@gmail.com>
2026-05-23 06:06:12 +0200

    Added render

---
+++ hypr/hyprland/render.lua
@@ -0,0 +1,8 @@
+hl.config({
+    render = {
+        -- Sends frames directly to the GPU instead of Hyprland
+        -- Reduces latency but can introduce issues
+        -- 0 - off, 1 - on, 2 - auto (on with content type ‘game’)
+        direct_scanout = 2,
+    }
+})
