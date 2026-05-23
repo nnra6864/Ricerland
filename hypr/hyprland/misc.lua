dbe44783173dcfd9c39a1068789612f029084da9
nnra6864 <nnra2210@gmail.com>
2026-05-23 06:05:56 +0200

    Added misc

---
+++ hypr/hyprland/misc.lua
@@ -0,0 +1,25 @@
+local defs = require("defs")
+
+hl.config({
+    misc = {
+        disable_hyprland_logo    = true,
+        disable_splash_rendering = true,
+
+        font_family        = defs.font.family,
+        splash_font_family = defs.font.family,
+
+        col = { splash     = defs.theme.color.foreground },
+        background_color   = defs.theme.color.background,
+
+        animate_manual_resizes       = defs.animations.animate_manual_resizes,
+        animate_mouse_windowdragging = defs.animations.animate_mouse_windowdragging,
+
+        vrr                        = false,
+        focus_on_activate          = false,
+        session_lock_xray          = false,
+        close_special_on_empty     = false,
+        initial_workspace_tracking = 0,
+        middle_click_paste         = false,
+        enable_anr_dialog          = false,
+    }
+})
