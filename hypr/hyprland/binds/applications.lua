503c0a5ea7cbdf545b68ff9b2fe34a7328b08dfa
nnra6864 <nnra2210@gmail.com>
2026-05-23 06:03:09 +0200

    Updated application binds

--- hypr/hyprland/binds/applications.lua
+++ hypr/hyprland/binds/applications.lua
@@ -10,4 +10,14 @@ hl.bind(defs.main_mod .. "+ Q",                hl.dsp.exec_cmd("xdg-terminal-exe
 hl.bind(defs.main_mod .. "+ E",                hl.dsp.exec_cmd("xdg-terminal-exec yazi"))
 hl.bind(defs.main_mod .. "+ ESCAPE",           hl.dsp.exec_cmd("xdg-terminal-exec btop"))
 hl.bind(defs.main_mod .. "+ CTRL + P",         hl.dsp.exec_cmd("pkill hyprpicker || hyprpicker -a"))
+
+-- Lock
 hl.bind(defs.main_mod .. "+ CTRL + SHIFT + L", hl.dsp.exec_cmd("hyprlock"))
+hl.bind("switch:off:Lid Switch",               hl.dsp.exec_cmd("hyprlock"))
+
+-- Global
+hl.bind("F10", function()
+    hl.dispatch(hl.dsp.pass({ window = "class: com\\.obsproject\\.Studio" }))
+    hl.dispatch(hl.dsp.exec_cmd("notify-send -t 1000 'OBS' 'Instant Replay Saved'"))
+    hl.dispatch(hl.dsp.exec_cmd("paplay " .. defs.sound.instant_replay))
+end)
