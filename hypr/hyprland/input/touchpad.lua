e377f1ec54df3f1ae6324edd2e2a14fac67d10e7
nnra6864 <nnra2210@gmail.com>
2026-05-23 06:05:40 +0200

    touchpad

---
+++ hypr/hyprland/input/touchpad.lua
@@ -0,0 +1,9 @@
+hl.config({
+    input = {
+        touchpad = {
+            disable_while_typing = false,
+            natural_scroll       = false,
+            scroll_factor        = 1
+        }
+    }
+})
