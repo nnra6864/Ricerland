2c246569ed5e45364077bcb60319e613494524fe
nnra6864 <nnra2210@gmail.com>
2026-05-23 06:05:22 +0200

    Added input cfg

---
+++ hypr/hyprland/input/input.lua
@@ -0,0 +1,24 @@
+hl.config({
+    input = {
+        -- Keyboard
+        kb_model           = "",
+        kb_layout          = "us, ru, rs   , rs",
+        kb_variant         = "  ,   , latin,   ",
+        kb_options         = "",
+        kb_rules           = "",
+        numlock_by_default = false,
+        repeat_rate        = 25,
+        repeat_delay       = 300,
+
+        -- Mouse
+        sensitivity                 = 0,
+        accel_profile               = "flat",
+        left_handed                 = false,
+        scroll_factor               = 1,
+        natural_scroll              = false,
+        follow_mouse                = 1,
+        focus_on_close              = 0,
+        mouse_refocus               = true,
+        float_switch_override_focus = 1,
+    }
+})
