ba1ef1bfe80ab222bdc5d9149f546abf3f0d535e
nnra6864 <nnra2210@gmail.com>
2026-05-23 06:04:52 +0200

    env formatting

--- hypr/hyprland/env/init.lua
+++ hypr/hyprland/env/init.lua
@@ -1,4 +1,5 @@
 require("hyprland.env.hyprland")
 require("hyprland.env.applications")
 require("hyprland.env.appearance")
+-- TODO: Only load if nvidia gpu is detected
 require("hyprland.env.nvidia")
