a6388b94ffcab8aa9aa2d93c973ac884ee4e0f61
nnra6864 <nnra2210@gmail.com>
2026-05-31 08:44:34 +0200

    Fixed scrolling

--- hypr/hyprland/layouts/scrolling.lua
+++ hypr/hyprland/layouts/scrolling.lua
@@ -3,7 +3,7 @@ hl.config({
         fullscreen_on_one_column = false,
         column_width             = 0.333,
         focus_fit_method         = 0,
-        follow_focus             = false,
+        follow_focus             = true,
         follow_min_visible       = 1,
         explicit_column_widths   = "0.25, 0.333, 0.5, 0.667, 0.75, 1.0",
         wrap_focus               = true,
