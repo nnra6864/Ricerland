a3ba5443691784528e81256ff7613a98cbb3e9aa
nnra6864 <nnra2210@gmail.com>
2026-05-31 06:47:00 +0200

    Added scrolling layout

---
+++ hypr/hyprland/layouts/init.lua
@@ -0,0 +1,1 @@
+require("hyprland/layouts/scrolling")
---
+++ hypr/hyprland/layouts/scrolling.lua
@@ -0,0 +1,13 @@
+hl.config({
+    scrolling = {
+        fullscreen_on_one_column = false,
+        column_width             = 0.333,
+        focus_fit_method         = 0,
+        follow_focus             = false,
+        follow_min_visible       = 1,
+        explicit_column_widths   = "0.25, 0.333, 0.5, 0.667, 0.75, 1.0",
+        wrap_focus               = true,
+        wrap_swapcol             = true,
+        direction                = "right"
+    }
+})
