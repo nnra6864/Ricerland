a85e112886e6ea1a417b4a0737524c90cdbf1217
nnra6864 <nnra2210@gmail.com>
2026-05-31 06:46:53 +0200

    Added QT_STYLE_OVERRIDE to appearance.lua

--- hypr/hyprland/env/appearance.lua
+++ hypr/hyprland/env/appearance.lua
@@ -6,6 +6,7 @@ hl.env("GDK_SCALE", defs.theme.gtk.scaling)
 
 -- QT
 hl.env("QT_QPA_PLATFORMTHEME",                defs.theme.qt.platform_theme)
+hl.env("QT_STYLE_OVERRIDE",                   defs.theme.qt.style)
 hl.env("QT_QUICK_CONTROLS_STYLE",             defs.theme.qt.quick_controls_style)
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", defs.theme.qt.disable_window_decoration)
 hl.env("QT_SCALE_FACTOR",                     defs.theme.qt.scaling)
