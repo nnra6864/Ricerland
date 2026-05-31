9f989297a7e35907fdc0bac0580e08f50d1663aa
nnra6864 <nnra2210@gmail.com>
2026-05-31 16:35:00 +0200

    Fixed startup

--- hypr/hyprland/startup.lua
+++ hypr/hyprland/startup.lua
@@ -1,6 +1,7 @@
 local defs = require("defs")
 
 hl.on("hyprland.start", function()
+    hl.exec_cmd("dunst")
     hl.exec_cmd("sunshine")
     hl.exec_cmd("pypr")
     hl.exec_cmd("vicinae server")
@@ -13,20 +14,28 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("fish -c 'track_app_usage'")
 
     -- Main
-    local main_workspace = "1"
+    local main_workspace = "1 silent"
     hl.exec_cmd(defs.apps.browser, { workspace = main_workspace })
 
     -- Social
-    local social_workspace = "2"
+    local social_workspace = "2 silent"
     hl.exec_cmd("mumble",                              { workspace = social_workspace })
-    hl.exec_cmd("sleep 1 && flatpak run im.riot.Riot", { workspace = social_workspace })
+    hl.exec_cmd("sleep 2 && flatpak run im.riot.Riot", { workspace = social_workspace })
 
     -- Special
     local special_workspace = "special"
-    hl.exec_cmd("keepassxc",                                        { workspace = special_workspace })
-    hl.exec_cmd("sleep 1 && flatpak run md.obsidian.Obsidian",      { workspace = special_workspace })
-    hl.exec_cmd("xdg-terminal-exec ncmpcpp",                        { workspace = special_workspace })
-    hl.exec_cmd("obs --startreplaybuffer --disable-shutdown-check", { workspace = special_workspace })
+    hl.exec_cmd("keepassxc",                                        { workspace = special_workspace .. " silent" })
+    hl.exec_cmd("obs --startreplaybuffer --disable-shutdown-check", { workspace = special_workspace .. " silent" })
+    hl.exec_cmd("flatpak run md.obsidian.Obsidian",                 { workspace = special_workspace .. " silent" })
+    hl.exec_cmd("xdg-terminal-exec ncmpcpp",                        { workspace = special_workspace .. " silent" })
 
-    hl.exec_cmd("hyprlock")
+    -- Move broken apps manually
+    hl.exec_cmd(string.format(
+        "sleep 2 && hyprctl dispatch 'hl.dsp.window.move({ workspace = \"%s\", follow = false, window = \"class:obsidian\" })'",
+        special_workspace))
+    hl.exec_cmd(string.format(
+        "sleep 2.5 && hyprctl dispatch 'hl.dsp.window.move({ workspace = \"%s\", follow = false, window = \"title:ncmpcpp\" })'",
+        special_workspace))
+
+    hl.exec_cmd("sleep 3 && hyprlock")
 end)
