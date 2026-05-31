9f1542026f4f8ab79c0f72c267381ce606e6ed78
nnra6864 <nnra2210@gmail.com>
2026-05-31 09:12:52 +0200

    startup.lua

--- hypr/hyprland/startup.lua
+++ hypr/hyprland/startup.lua
@@ -12,7 +12,21 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("fish -c 'track_app_usage'")
 
-    hl.exec_cmd(defs.apps.terminal)
-    hl.exec_cmd(defs.apps.browser)
-    hl.exec_cmd(defs.apps.terminal)
+    -- Main
+    local main_workspace = "1"
+    hl.exec_cmd(defs.apps.browser, { workspace = main_workspace })
+
+    -- Social
+    local social_workspace = "2"
+    hl.exec_cmd("mumble",                              { workspace = social_workspace })
+    hl.exec_cmd("sleep 1 && flatpak run im.riot.Riot", { workspace = social_workspace })
+
+    -- Special
+    local special_workspace = "special"
+    hl.exec_cmd("keepassxc",                                        { workspace = special_workspace })
+    hl.exec_cmd("sleep 1 && flatpak run md.obsidian.Obsidian",      { workspace = special_workspace })
+    hl.exec_cmd("xdg-terminal-exec ncmpcpp",                        { workspace = special_workspace })
+    hl.exec_cmd("obs --startreplaybuffer --disable-shutdown-check", { workspace = special_workspace })
+
+    hl.exec_cmd("hyprlock")
 end)
