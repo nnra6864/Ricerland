53944390d3aab3e6600b4f1486540d0b0419d8bf
nnra6864 <nnra2210@gmail.com>
2026-05-31 16:14:21 +0200

    Exporting env, actually needed

--- hypr/hyprland/env/init.lua
+++ hypr/hyprland/env/init.lua
@@ -3,3 +3,8 @@ require("hyprland.env.applications")
 require("hyprland.env.appearance")
 -- TODO: Only load if nvidia gpu is detected
 require("hyprland.env.nvidia")
+
+-- Export env
+hl.on("hyprland.start", function()
+    hl.exec_cmd("dbus-update-activation-environment --systemd --all")
+end)
