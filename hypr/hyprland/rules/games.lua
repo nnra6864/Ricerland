d074898ede7a9ce69ea30fde0e9b24e3147756f5
nnra6864 <nnra2210@gmail.com>
2026-05-31 16:34:34 +0200

    Added game rules

---
+++ hypr/hyprland/rules/games.lua
@@ -0,0 +1,15 @@
+hl.window_rule({ match = { class = "cs2"},           tag = "+game" })
+hl.window_rule({ match = { class = "csgo_linux64"},  tag = "+game" })
+hl.window_rule({ match = { class = "steam_app_730"}, tag = "+game" })
+hl.window_rule({ match = { class = "hl_linux"},      tag = "+game" })
+hl.window_rule({ match = { class = "osu! # Osu"},    tag = "+game" })
+hl.window_rule({ match = { class = "Beat Saber"},    tag = "+game" })
+
+hl.window_rule({
+    name            = "games",
+    match           = { tag = "game" },
+    fullscreen      = true,
+    opaque          = true,
+    immediate       = true,
+    confine_pointer = true
+})
