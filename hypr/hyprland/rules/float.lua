8752eaf5fe43eb90ec7b90022f7aeae59f200835
nnra6864 <nnra2210@gmail.com>
2026-05-31 16:34:30 +0200

    Added float rules

---
+++ hypr/hyprland/rules/float.lua
@@ -0,0 +1,12 @@
+hl.window_rule({ match = { class = "waydroid.com.*"},                                                 tag = "+float" })
+hl.window_rule({ match = { class = "hyprland-share-picker"},                                          tag = "+float" })
+hl.window_rule({ match = { class = "org.keepassxc.KeePassXC", title = "KeePassXC - Access Request"},  tag = "+float" })
+hl.window_rule({ match = { class = "org.keepassxc.KeePassXC", title = "Unlock Database - KeePassXC"}, tag = "+float" })
+hl.window_rule({ match = { class = "Material Maker",          title = "Alert!"},                      tag = "+float" })
+
+hl.window_rule({
+    name   = "float",
+    match  = { tag = "float" },
+    center = true,
+    float  = true,
+})
