6b69d62e06dedc6e9e5c20242364849535d33a68
nnra6864 <nnra2210@gmail.com>
2026-05-31 16:14:29 +0200

    Disabled direct_scanout

--- hypr/hyprland/render.lua
+++ hypr/hyprland/render.lua
@@ -3,6 +3,6 @@ hl.config({
         -- Sends frames directly to the GPU instead of Hyprland
         -- Reduces latency but can introduce issues
         -- 0 - off, 1 - on, 2 - auto (on with content type ‘game’)
-        direct_scanout = 2,
+        direct_scanout = 0,
     }
 })
