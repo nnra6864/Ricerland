60cdf3f16177d1f1fcf5e8ff09a9591f696b7d33
nnra6864 <nnra2210@gmail.com>
2026-06-01 05:59:46 +0200

    Using the iterator in rules and added slide top for notifications

--- hypr/hyprland/rules/layers.lua
+++ hypr/hyprland/rules/layers.lua
@@ -1,16 +1,28 @@
--- TODO: Iterate instead of using regex
+local layers = {
+    "notifications",
+    "wlr_which_key",
+    "vicinae",
+    "hyprlauncher",
+    "rofi",
+    "swaync-control-center"
+}
+
+for _, layer in ipairs(layers) do
+    hl.layer_rule({
+        match = {
+            namespace = layer
+        },
+        blur         = true,
+        xray         = true,
+        blur_popups  = true,
+        ignore_alpha = 0.1
+    })
+end
+
+-- Notifications should slide from top
 hl.layer_rule({
     match = {
-        namespace =
-            "notifications" ..
-            "|wlr_which_key" ..
-            "|vicinae"..
-            "|hyprlauncher" ..
-            "|rofi" ..
-            "|swaync-control-center"
+        namespace = "notifications",
     },
-    blur         = true,
-    xray         = true,
-    blur_popups  = true,
-    ignore_alpha = 0.1
+    animation = "slide top"
 })
