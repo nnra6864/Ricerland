ce1bdffad02ce34e7dbfe129b3d6d5e9c7daea51
nnra6864 <nnra2210@gmail.com>
2026-06-02 21:41:57 +0200

    Renamed and restructured csgo-vulkan-fix

--- hypr/hyprland/plugins/csgo-vulkan-fix.lua
+++
@@ -1,7 +0,0 @@
-if hl.plugin.csgo_vulkan_fix then
-    local p = hl.plugin.csgo_vulkan_fix
-
-    p.vkfix_app({ app = "hl_linux",      w = 1280, h = 1024 })
-    p.vkfix_app({ app = "csgo_linux64",  w = 1280, h = 1024 })
-    p.vkfix_app({ app = "steam_app_730", w = 1280, h = 1024 })
-end
