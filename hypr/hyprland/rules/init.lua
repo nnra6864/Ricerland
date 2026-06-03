6a9735d9fb149a646309728e509947c516696595
nnra6864 <nnra2210@gmail.com>
2026-06-03 03:04:02 +0200

    Added some nice rules

--- hypr/hyprland/rules/init.lua
+++ hypr/hyprland/rules/init.lua
@@ -1,3 +1,26 @@
+-- Ignore maximize requests from all apps
+hl.window_rule({
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+
+    suppress_event = "maximize",
+})
+
+-- Fix some dragging issues with XWayland
+hl.window_rule({
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+
+    no_focus = true,
+})
+
 require("hyprland.rules.float")
 require("hyprland.rules.games")
 require("hyprland.rules.layers")
