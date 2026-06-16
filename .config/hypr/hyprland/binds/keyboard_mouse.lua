3690f2b2f5f5118124f79e2dd812563b115f1f01
nnra6864 <nnra2210@gmail.com>
2026-06-16 05:15:26 +0200

    Got rid of keyboard_mouse, I'll make that with zig

--- .config/hypr/hyprland/binds/keyboard_mouse.lua
+++
@@ -1,8 +0,0 @@
-local defs = require("defs")
-
-hl.bind(defs.main_mod "+ M", hl.dsp.submap("keyboard_mouse"))
-
-hl.define_submap("keyboard_mouse", function()
-
-    hl.bind("escape", hl.dsp.submap("reset"))
-end)
