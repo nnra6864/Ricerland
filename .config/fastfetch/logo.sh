8a6d4006eb41b42228167bd3428428a4a56239a4
nnra6864 <nnra2210@gmail.com>
2026-07-10 19:07:08 +0200

    Reworked the fastfetch config completely

--- .config/fastfetch/logo.sh
+++ .config/fastfetch/logo.sh
@@ -3,27 +3,33 @@
 # Fixes inconsistent logo width
 export LC_ALL=en_US.UTF-8
 
-# NN
-cat "$(dirname "$0")/NN"
+# Variables
+LOGO="$(dirname "$0")/eastern_orthodox_logo"
+LOGO_WIDTH=$(wc -L < "$LOGO")
+COLOR_SYMBOL="፠፠፠"
+COLOR_WIDTH=$(($(printf $COLOR_SYMBOL | wc -m) * 8))
+
+# Padding
+if [ $LOGO_WIDTH -gt $COLOR_WIDTH ]; then
+    LOGO_PADDING=0
+    COLOR_PADDING=$(( (LOGO_WIDTH - COLOR_WIDTH) / 2 ))
+elif [ $COLOR_WIDTH -gt $LOGO_WIDTH ]; then
+    LOGO_PADDING=$(( (COLOR_WIDTH - LOGO_WIDTH) / 2 ))
+    COLOR_PADDING=0
+else
+    LOGO_PADDING=0
+    COLOR_PADDING=0
+fi
+
+# Spaces
+LOGO_SPACES=$(printf "%*s" $LOGO_PADDING "")
+COLOR_SPACES=$(printf "%*s" $COLOR_PADDING "")
+
+# Logo
+sed "s/^/$LOGO_SPACES/" "$LOGO"
 echo ""
-echo ""
-echo ""
-
-# Get the width of the first line of the logo
-LOGO_WIDTH=$(head -n1 "$(dirname "$0")/NN" | wc -m)
-# Color bar width is 32 characters (8 colors × 4 chars each including color codes)
-COLOR_WIDTH=32
-
-# Right
-PADDING=$((LOGO_WIDTH - COLOR_WIDTH - 1))
-
-# Center
-#PADDING=$(((LOGO_WIDTH / 2) - (COLOR_WIDTH/ 2)))
-
-# Add padding spaces before the color bars
-SPACES=$(printf "%*s" $PADDING "")
 
 # Regular colors
-echo -e "${SPACES}\033[30m████\033[31m████\033[32m████\033[33m████\033[34m████\033[35m████\033[36m████\033[37m████\033[0m"
+echo -e "${COLOR_SPACES}\033[30m$COLOR_SYMBOL\033[31m$COLOR_SYMBOL\033[32m$COLOR_SYMBOL\033[33m$COLOR_SYMBOL\033[34m$COLOR_SYMBOL\033[35m$COLOR_SYMBOL\033[36m$COLOR_SYMBOL\033[37m$COLOR_SYMBOL\033[0m"
 # Bright colors
-echo -e "${SPACES}\033[90m████\033[91m████\033[92m████\033[93m████\033[94m████\033[95m████\033[96m████\033[97m████\033[0m"
+echo -e "${COLOR_SPACES}\033[90m$COLOR_SYMBOL\033[91m$COLOR_SYMBOL\033[92m$COLOR_SYMBOL\033[93m$COLOR_SYMBOL\033[94m$COLOR_SYMBOL\033[95m$COLOR_SYMBOL\033[96m$COLOR_SYMBOL\033[97m$COLOR_SYMBOL\033[0m"
