50c0fa13a87e4f643c7b51cb3dc20ee3d53af647
nnra6864 <nnra2210@gmail.com>
2026-07-30 13:18:41 +0200

    Updating zoxide db on yazi cwd

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,10 @@
 require("full-border"):setup()
 
+-- Update zoxide on CWD
+require("zoxide"):setup {
+	update_db = true,
+}
+
 require("git"):setup {
 	-- Order of status signs showing in the linemode
 	order = 1500,
