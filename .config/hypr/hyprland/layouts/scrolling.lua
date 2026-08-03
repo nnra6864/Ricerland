1b12f3fb8c973a7788d91a5181d93871b8739924
nnra6864 <nnra2210@gmail.com>
2026-08-03 17:06:21 +0200

    Adjusted scrolling width so it fits on screen

--- .config/hypr/hyprland/layouts/scrolling.lua
+++ .config/hypr/hyprland/layouts/scrolling.lua
@@ -1,11 +1,11 @@
 hl.config({
     scrolling = {
         fullscreen_on_one_column = false,
-        column_width             = 0.5,
+        column_width             = 0.499,
         focus_fit_method         = 0,
         follow_focus             = true,
         follow_min_visible       = 1,
-        explicit_column_widths   = "0.25, 0.333, 0.5, 0.667, 0.75, 1.0",
+        explicit_column_widths   = "0.249, 0.332, 0.499, 0.665, 0.749, 1.0",
         wrap_focus               = true,
         wrap_swapcol             = true,
         direction                = "right"
