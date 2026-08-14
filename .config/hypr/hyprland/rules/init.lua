1cedf2fe8fa53e9fa76859ca89a8cc9b3aeb7516
nnra6864 <nnra2210@gmail.com>
2026-08-14 02:17:24 +0200

    Added espanso

--- .config/hypr/hyprland/rules/init.lua
+++ .config/hypr/hyprland/rules/init.lua
@@ -21,6 +21,23 @@ hl.window_rule({
     no_focus = true,
 })
 
+-- Fix espanso config reload stealing focus
+hl.window_rule({
+    name  = "espanso",
+    match = {
+        class = "Espanso.SyncTool",
+    },
+
+    float             = true,
+    opacity           = 0.001,
+    size              = { "monitor_w", "monitor_h" },
+    focus_on_activate = true,
+
+    decorate = false,
+    no_anim  = true,
+    no_blur  = true,
+    no_dim   = true,
+})
 require("hyprland.rules.float")
 require("hyprland.rules.games")
 require("hyprland.rules.layers")
