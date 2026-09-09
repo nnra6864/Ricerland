584a58063767fbb635a580f6c80887f09580c887
nnra6864 <nnra2210@gmail.com>
2026-09-09 08:06:47 +0200

    feat: Added a whatsmyip alias to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,6 +17,7 @@ alias lg='lazygit'
 alias lnw='sh ~/.config/hypr/HyprlandUnityFix/ListNewWindows.sh'
 alias tm='tmux new -As'
 alias ls='zl -la --dir-grouping=before --no-permissions --no-group -C ~/.config/zlist/zlist.zon'
+alias wmip='curl ifconfig.me --silent | wl-copy'
 
 # Paths
 alias cfg='cd ~/.config/'
